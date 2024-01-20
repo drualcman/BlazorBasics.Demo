efec3dd7d54e9affdaa570cda2a41c25e65c1bcc
Sergi Ortiz Gómez <drualcman@msn.com>
2024-01-20 08:15:35 +0800

    Add project files.

---
+++ BlazorBasics.Demo.ContactForm/BlazorBasics.Demo.ContactForm.Client/Models/ContactFormModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorBasics.Demo.ContactForm.Client.Models;
+
+public class ContactFormModel
+{
+    public string Email { get; set; }
+    public string Comments { get; set; }
+    public string FileName { get; set; }
+    public Stream FileStream { get; set; }
+}
